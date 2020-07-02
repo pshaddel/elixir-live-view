6b5dedb20da13fb0e37e16e862b8baee75d2a707
Mike Clark <mike@clarkware.com>
2020-07-02 14:35:23 -0600

    Add VolunteersLive and generated files for managing volunteers

--- lib/live_view_studio_web/router.ex
+++ lib/live_view_studio_web/router.ex
@@ -27,6 +27,7 @@ defmodule LiveViewStudioWeb.Router do
     live "/servers", ServersLive
     live "/paginate", PaginateLive
     live "/sort", SortLive
+    live "/volunteers", VolunteersLive
   end
 
   # Other scopes may use custom stacks.
