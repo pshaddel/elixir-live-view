ae453c0157f1f8946f73d620d0aa18b9eb0afdaa
Mike Clark <mike@clarkware.com>
2020-07-02 15:52:51 -0600

    Finish live create for VolunteersLive

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -7,11 +7,38 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        changeset: changeset
       )
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [volunteers: []]}
+  end
+
+  def handle_event("save", %{"volunteer" => params}, socket) do
+    case Volunteers.create_volunteer(params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        socket = assign(socket, changeset: changeset)
+
+        :timer.sleep(500)
+
+        {:noreply, socket}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = assign(socket, changeset: changeset)
+        {:noreply, socket}
+    end
   end
 end
