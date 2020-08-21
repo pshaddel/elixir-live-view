95f131373dc87e7afe6dce20a3d008068868d284
Mike Clark <mike@clarkware.com>
2020-08-21 12:34:15 -0600

    Finish SandboxLive

---
+++ lib/live_view_studio_web/live/components/quote_component.ex
@@ -0,0 +1,31 @@
+defmodule LiveViewStudioWeb.QuoteComponent do
+  use LiveViewStudioWeb, :live_component
+
+  import Number.Currency
+
+  def mount(socket) do
+    {:ok, assign(socket, hrs_until_expires: 24)}
+  end
+
+  # def update(assigns, socket) do
+  #   socket = assign(socket, assigns)
+  #   {:ok, socket}
+  # end
+
+  def render(assigns) do
+    ~L"""
+    <div class="text-center p-6 border-4 border-dashed border-indigo-600">
+      <h2 class="text-2xl mb-2">
+        Our Best Deal:
+      </h2>
+      <h3 class="text-xl font-semibold text-indigo-600">
+        <%= @weight %> pounds of <%= @material %>
+        for <%= number_to_currency(@price) %>
+      </h3>
+      <div class="text-gray-600">
+        expires in <%= @hrs_until_expires %> hours
+      </div>
+    </div>
+    """
+  end
+end
--- lib/live_view_studio_web/live/sandbox_live.ex
+++ lib/live_view_studio_web/live/sandbox_live.ex
@@ -1,8 +1,11 @@
 defmodule LiveViewStudioWeb.SandboxLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.QuoteComponent
+  alias LiveViewStudioWeb.SandboxCalculatorComponent
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, weight: nil, price: nil)}
   end
 
   def render(assigns) do
@@ -10,7 +13,22 @@ defmodule LiveViewStudioWeb.SandboxLive do
     <h1>Build A Sandbox</h1>
 
     <div id="sandbox">
+      <%= live_component @socket, SandboxCalculatorComponent,
+                         id: 1,
+                         coupon: 10.0 %>
+
+      <%= if @weight do %>
+        <%= live_component @socket, QuoteComponent,
+                          material: "sand",
+                          weight: @weight,
+                          price: @price %>
+      <% end %>
     </div>
     """
+  end
+
+  def handle_info({:totals, weight, price}, socket) do
+    socket = assign(socket, weight: weight, price: price)
+    {:noreply, socket}
   end
 end
