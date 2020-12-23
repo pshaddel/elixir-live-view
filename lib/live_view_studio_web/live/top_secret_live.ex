3bb8318de7fba09eb65fd9a3b42a1389416060aa
Mike Clark <mike@clarkware.com>
2020-12-23 11:39:12 -0700

    Add initial TopSecretLive and supporting CSS

---
+++ lib/live_view_studio_web/live/top_secret_live.ex
@@ -0,0 +1,26 @@
+defmodule LiveViewStudioWeb.TopSecretLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Top Secret</h1>
+    <div id="top-secret">
+      <img src="images/spy.svg">
+      <div class="mission">
+        <h2>Your Mission</h2>
+        <h3>001</h3>
+        <p>
+          (should you choose to accept it)
+        </p>
+        <p>
+          is detailed below...
+        </p>
+      </div>
+    </div>
+    """
+  end
+end
