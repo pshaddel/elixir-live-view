41c7056df93cbaf043f7487551c759df42805d63
Mike Clark <mike@clarkware.com>
2021-01-25 15:04:53 -0700

    Finish ChartLive

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,4 +1,17 @@
+import LineChart from "./line-chart"
+
 let Hooks = {};
+
+Hooks.LineChart = {
+  mounted() {
+    const { labels, values } = JSON.parse(this.el.dataset.chartData)
+    this.chart = new LineChart(this.el, labels, values)
+
+    this.handleEvent("new-point", ({ label, value }) => {
+      this.chart.addPoint(label, value)
+    })
+  }
+}
 
 Hooks.InfiniteScroll = {
   mounted() {
--- lib/live_view_studio_web/live/chart_live.ex
+++ lib/live_view_studio_web/live/chart_live.ex
@@ -2,16 +2,66 @@ defmodule LiveViewStudioWeb.ChartLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :update)
+    end
+
+    labels = 1..12 |> Enum.to_list()
+
+    values = Enum.map(labels, fn _ -> get_reading() end)
+
+    {:ok,
+     assign(socket,
+       chart_data: %{
+         labels: labels,
+         values: values
+       },
+       current_reading: List.last(labels)
+     )}
   end
 
   def render(assigns) do
     ~L"""
     <div id="charting">
       <h1>Blood Sugar</h1>
-      <canvas id="chart-canvas">
-      </canvas>
+      <div phx-update="ignore">
+        <canvas id="chart-canvas"
+                phx-hook="LineChart"
+                data-chart-data="<%= Jason.encode!(@chart_data) %>">
+        </canvas>
+      </div>
+      <div class="text-center">
+        <button phx-click="get-reading">
+          Get Reading
+        </button>
+        <div class="my-4">
+          Total readings: <%= @current_reading %>
+        </div>
+      </div>
     </div>
     """
+  end
+
+  def handle_info(:update, socket) do
+    {:noreply, add_point(socket)}
+  end
+
+  def handle_event("get-reading", _, socket) do
+    {:noreply, add_point(socket)}
+  end
+
+  defp add_point(socket) do
+    socket = update(socket, :current_reading, &(&1 + 1))
+
+    point = %{
+      label: socket.assigns.current_reading,
+      value: get_reading()
+    }
+
+    push_event(socket, "new-point", point)
+  end
+
+  defp get_reading do
+    Enum.random(70..180)
   end
 end
