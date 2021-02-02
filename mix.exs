edd33b3f92c65510e89259fa523d90100832e7b0
Mike Clark <mike@clarkware.com>
2021-02-02 12:10:36 -0700

    Update faker gem version

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule LiveViewStudio.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:faker, "~> 0.14.0"},
+      {:faker, "~> 0.16.0"},
       {:number, "~> 1.0"},
       {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false}
     ]
