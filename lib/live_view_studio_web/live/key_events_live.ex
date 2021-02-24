42dc9368fab6d978e9bab4b41d22c6e03ee146d8
Mike Clark <mike@clarkware.com>
2021-02-24 16:32:19 -0700

    Finish KeyEventsLive

--- lib/live_view_studio_web/live/key_events_live.ex
+++ lib/live_view_studio_web/live/key_events_live.ex
@@ -5,20 +5,65 @@ defmodule LiveViewStudioWeb.KeyEventsLive do
     {:ok,
      assign(socket,
        images: Enum.map(0..18, &"juggling-#{&1}.jpg"),
-       current: 0
+       current: 0,
+       is_playing: false,
+       timer: nil
      )}
   end
 
   def render(assigns) do
     ~L"""
     <h1>Juggling Key Events</h1>
-    <div id="key-events">
+    <div id="key-events" phx-window-keyup="update">
       <img src="/images/juggling/<%= Enum.at(@images, @current) %>">
       <div class="status">
         <%= Enum.at(@images, @current) %>
+        <input type="number" value="<%= @current %>"
+               phx-keyup="set-current"
+               phx-key="Enter" />
+        <div>
+          <%= if @is_playing, do: "Playing", else: "Paused" %>
+        </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("set-current", %{"key" => "Enter", "value" => value}, socket) do
+    {:noreply, assign(socket, :current, String.to_integer(value))}
+  end
+
+  def handle_event("update", %{"key" => "ArrowRight"}, socket) do
+    {:noreply, assign(socket, :current, next(socket))}
+  end
+
+  def handle_event("update", %{"key" => "ArrowLeft"}, socket) do
+    {:noreply, assign(socket, :current, previous(socket))}
+  end
+
+  def handle_event("update", %{"key" => "k"}, socket) do
+    socket = update(socket, :is_playing, fn playing -> !playing end)
+
+    socket =
+      if socket.assigns.is_playing do
+        {:ok, timer} = :timer.send_interval(250, self(), :tick)
+        assign(socket, :timer, timer)
+      else
+        :timer.cancel(socket.assigns.timer)
+        assign(socket, :timer, nil)
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("update", %{"key" => key}, socket) do
+    IO.inspect(key)
+    {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    socket = assign(socket, :current, next(socket))
+    {:noreply, socket}
   end
 
   defp next(socket) do
