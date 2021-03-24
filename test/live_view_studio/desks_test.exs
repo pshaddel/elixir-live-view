d551a11e337b555c20017a0fd74cefb539512d41
Mike Clark <mike@clarkware.com>
2021-03-24 10:23:39 -0600

    Fix generated DesksTest

--- test/live_view_studio/desks_test.exs
+++ test/live_view_studio/desks_test.exs
@@ -2,20 +2,15 @@ defmodule LiveViewStudio.DesksTest do
   use LiveViewStudio.DataCase
 
   alias LiveViewStudio.Desks
+  alias LiveViewStudio.Desks.Desk
 
   describe "desks" do
-    alias LiveViewStudio.Desks.Desk
-
     @valid_attrs %{name: "some name", photo_urls: []}
     @update_attrs %{name: "some updated name", photo_urls: []}
     @invalid_attrs %{name: nil, photo_urls: nil}
 
     def desk_fixture(attrs \\ %{}) do
-      {:ok, desk} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Desks.create_desk()
-
+      {:ok, desk} = create_desk(Enum.into(attrs, @valid_attrs))
       desk
     end
 
@@ -30,13 +25,13 @@ defmodule LiveViewStudio.DesksTest do
     end
 
     test "create_desk/1 with valid data creates a desk" do
-      assert {:ok, %Desk{} = desk} = Desks.create_desk(@valid_attrs)
+      assert {:ok, %Desk{} = desk} = create_desk(@valid_attrs)
       assert desk.name == "some name"
       assert desk.photo_urls == []
     end
 
     test "create_desk/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Desks.create_desk(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = create_desk(@invalid_attrs)
     end
 
     test "update_desk/2 with valid data updates the desk" do
@@ -62,5 +57,9 @@ defmodule LiveViewStudio.DesksTest do
       desk = desk_fixture()
       assert %Ecto.Changeset{} = Desks.change_desk(desk)
     end
+  end
+
+  defp create_desk(attrs) do
+    Desks.create_desk(%Desk{}, attrs, &{:ok, &1})
   end
 end
